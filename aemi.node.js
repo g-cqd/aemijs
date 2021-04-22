fdae3f62f5fcf024ee4e5aa4e3e992c061371de9
Guillaume C <42325924+aemi-dev@users.noreply.github.com>
2021-04-22 16:58:25 +0200

    Fix aemi.node.js

--- aemi.node.js
+++ aemi.node.js
@@ -1,10 +1,10 @@
-const Utils = require( './node/utils.js' );
-const Dataset = require( './node/dataset.js' );
-const Functional = require( './node/functional.js' );
-const Loading = require( './node/loading.js' );
-const Math = require( './node/math.js' );
-const MultiThread = require( './node/multithread.js' );
-const Performance = require( './node/perf.js' );
+const Utils = require( './src/utils.js' );
+const Dataset = require( './src/dataset.js' );
+const Functional = require( './src/functional.js' );
+const Loading = require( './src/loading.js' );
+const Math = require( './src/math.js' );
+const MultiThread = require( './src/multithread.js' );
+const Performance = require( './src/perf.js' );
 
 module.exports = {
     Utils,
